5bd8282b0d5760c345346fa9acf9d1605b133261
NeuralPsy <kirill.neuralpsy@gmail.com>
2022-12-03 16:32:06 +0200

    Add 2 application tests cases

---
+++ src/test/java/il/neuralpsy/edustuff/EdustuffApplicationTests.java
@@ -0,0 +1,62 @@
+package il.neuralpsy.edustuff;
+
+import il.neuralpsy.edustuff.controller.UserController;
+import il.neuralpsy.edustuff.model.User;
+import il.neuralpsy.edustuff.model.UserType;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class EdustuffApplicationTests {
+
+	@Autowired
+	private final UserController userController;
+
+
+
+	@Test
+	void shouldAddUser() {
+		User maria = createUser1();
+		int userId = userController.addUser(maria).getUserId();
+		User userFromDB = userController.getUserById(userId).get();
+		assertThat(userFromDB.getUserId()).isEqualTo(userId);
+		assertThat(userFromDB.getName()).isEqualTo("Maria");
+		assertThat(userFromDB.getBirthdate()).isEqualTo("1990-02-03");
+	}
+
+	@Test
+	void shouldFindUserByEmail() {
+		User professor = createUser2();
+		User userFromDB = userController.addUser(professor);
+		assertThat(userFromDB.getEmail()).isEqualTo(professor.getEmail());
+	}
+
+	private User createUser1() {
+		User user = new User();
+		user.setUserType(new UserType(1, "STUDENT"));
+		user.setName("Maria");
+		user.setBirthdate(LocalDate.parse("1990-02-03"));
+		user.setEmail("pumpkin@gmail.com");
+		return user;
+	}
+
+	private User createUser2() {
+		User user = new User();
+		user.setUserType(new UserType(2, "TEACHER"));
+		user.setName("Mr. Professor");
+		user.setBirthdate(LocalDate.parse("1976-11-09"));
+		user.setEmail("professor@yahoo.com");
+		return user;
+	}
+
+}
