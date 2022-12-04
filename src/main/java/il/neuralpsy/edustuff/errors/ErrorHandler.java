5213c60e3a5778068c37acc8be1cab21a358ff26
NeuralPsy <kirill.neuralpsy@gmail.com>
2022-12-05 00:50:04 +0200

    Add Error handlers

---
+++ src/main/java/il/neuralpsy/edustuff/errors/ErrorHandler.java
@@ -0,0 +1,37 @@
+package il.neuralpsy.edustuff.errors;
+
+import il.neuralpsy.edustuff.exception.*;
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(final RuntimeException e){
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({
+            SubjectDoesntExistException.class,
+            TaskDoesntExistException.class,
+            UserDoesntExistException.class,
+            UserEmailDoesntExistException.class,
+            TaskDoesntExistException.class,
+            CommentDoesntExistException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(final RuntimeException e){
+        return new ErrorResponse(e.getMessage());
+    }
+
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ErrorResponse handleOtherExceptions(final Throwable e){
+//        return new ErrorResponse(e.getMessage());
+//    }
+
+}
---
+++ src/main/java/il/neuralpsy/edustuff/errors/ErrorResponse.java
@@ -0,0 +1,13 @@
+package il.neuralpsy.edustuff.errors;
+
+public class ErrorResponse {
+    private final String error;
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
